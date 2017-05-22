1f9e9b5e4bacb4627d256faa4a16bee6f2e7564d
Kenneth André Martínez Molina <kennethandremm@gmail.com>
2017-05-21 20:04:47 -0600

    DELETED: trash code in LoginActivity

--- app/src/main/java/org/kennek/esalud/LoginActivity.java
+++ app/src/main/java/org/kennek/esalud/LoginActivity.java
@@ -52,46 +52,6 @@ public class LoginActivity extends AppCompatActivity {
         goLoginScreen();
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            // Successfully signed in
-            if (resultCode == ResultCodes.OK) {
-                Intent intent = new Intent(this, MainActivity.class);
-                startActivity(intent);
-                finish();
-                return;
-            } else {
-                // Sign in failed
-                if (response == null) {
-                    // User pressed back button
-                    showSnackbar(R.string.sign_in_cancelled);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    showSnackbar(R.string.no_internet_connection);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                    showSnackbar(R.string.unknown_error);
-                    return;
-                }
-            }
-            showSnackbar(R.string.unknown_sign_in_response);
-        }
-    }
-
-    public Snackbar showSnackbar(int responseString){
-        Snackbar snack = Snackbar.make(
-                (((Activity) this).findViewById(android.R.id.content)),
-                responseString, Snackbar.LENGTH_SHORT);
-        return snack;
-    }
     public void click(View v) {
         Intent intent;
         switch(v.getId()) {
