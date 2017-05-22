339148accad4e770a94aba968c53bdbe5072ffa2
Kenneth André Martínez Molina <kennethandremm@gmail.com>
2017-05-21 23:16:45 -0600

    ADDED: Request permission

--- app/src/main/java/org/kennek/esalud/SplashActivity.java
+++ app/src/main/java/org/kennek/esalud/SplashActivity.java
@@ -2,7 +2,10 @@ package org.kennek.esalud;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,8 +24,40 @@ public class SplashActivity extends AppCompatActivity {
 
     // Choose an arbitrary request code value
     private static final int RC_SIGN_IN = 123;
+
+
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 100;
+    private static final int REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION = 200;
+    private static final int REQUEST_READ_EXTERNAL_STORAGE_PERMISSION = 300;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {android.Manifest.permission.RECORD_AUDIO,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE};
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+            case REQUEST_WRITE_EXTERNAL_STORAGE_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+            case REQUEST_READ_EXTERNAL_STORAGE_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
